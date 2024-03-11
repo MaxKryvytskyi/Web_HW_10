b09f7ac4a5b50fc333383588b5c5a31a1f96641a
Max <maxkryvytskyiwork@gmail.com>
2024-03-11 17:57:54 +0200

    save

--- hw_project/quotes/templatetags/extract.py
+++ hw_project/quotes/templatetags/extract.py
@@ -1,22 +1,26 @@
 from bson import ObjectId
 from django import template
-from ..utils import connect, Authors
-# import logging
-
-# logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
-
+from ..utils import connect, Authors, Quotes
+from quotes.models import Tag
+from django.db.models import Count 
 register = template.Library()
 
 def get_author(id_):
-    # logging.debug("_"* 30)
-    # logging.debug(f"{id_.id}")
-    # logging.debug("_"* 30)
     author = Authors.objects(id=ObjectId(id_.id)).first()
-    # logging.debug(f"{author.fullname}")
     return author.fullname
 
 def get_top_ten_tags(quotes):
-    pass
+    # select tag_id, count(*), qt.name
+    # FROM public.quotes_quote_tags
+    # join public.quotes_tag as qt on qt.id = tag_id 
+    # GROUP BY tag_id, qt.name;
 
-register.filter("quotes", get_top_ten_tags)
+    # Запрос на топ 10 тегов
+    top_tags = Tag.objects.annotate(quote_count=Count('quote')).order_by('-quote_count')[:10]
+    return top_tags
+
+def get_author_details(author):
+    return 
+
+register.filter("tags", get_top_ten_tags)
 register.filter("author", get_author)
